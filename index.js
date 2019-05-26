81bdf5255a49bf6866ee7f1687af063bc0027ab2
Sangje Park <sangjep@sfu.ca>
2019-05-25 19:53:52 -0700

    last check for font

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.post('/login', function(req, res){
-	//request body info
-	//req.body
+app.get('/', function(req, res) {
+    res.render('pages/index')
 })
+app.get('/histogram', function(req, res) {
+    res.sendFile('public/histogram.html', { root: __dirname })
+})
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
